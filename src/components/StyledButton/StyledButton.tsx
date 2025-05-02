bd61070efd3511171843ec3a7cd9bc5bb3d51e65
Andrei <andrei.rezende@hotmail.com>
2025-05-01 23:30:06 -0300

    creates styled button component

---
+++ src/components/StyledButton/StyledButton.tsx
@@ -0,0 +1,88 @@
+import { styled } from "@mui/material";
+import { ReactNode } from "react";
+import { keyframes } from "@emotion/react";
+
+interface StyledButtonProps {
+  children: ReactNode;
+}
+
+const halftone = keyframes`
+  0% {
+    background-size: 1.25em 1.25em;
+  }
+  50% {
+    background-size: 1.5em 1.5em;
+  }
+  100% {
+    background-size: 2.375em 2.375em, 0.1em 0.1em;
+  }
+`;
+
+
+const CustomButton = styled("button")(({ theme }) => ({
+  backgroundColor: "transparent",
+  border: `2px solid ${theme.palette.secondary.main}`,
+  borderBottomWidth: "4px",
+  borderRadius: "3px",
+  padding: "1em 2em",
+  color: theme.palette.secondary.main,
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "7px",
+  font: "inherit",
+  letterSpacing: "inherit",
+  textTransform: "inherit",
+  margin: "0.5em",
+  cursor: "pointer",
+  transition: "color 0.5s, background-color 0.5s, border-color 0.5s",
+  position: "relative",
+  overflow: "hidden",
+
+  '&::before': {
+    content: '""',
+    position: "absolute",
+    inset: 0,
+    backgroundImage: `
+      radial-gradient(circle, ${theme.palette.secondary.main} 0.2em, transparent 0.25em),
+      radial-gradient(circle, ${theme.palette.secondary.main} 0.2em, transparent 0.25em)
+    `,
+    backgroundPosition: "0 0, 6.25em 6.25em",
+    backgroundSize: "1.25em 1.25em",
+    zIndex: 0,
+    opacity: 0,
+    transition: "opacity 0.5s, background-size 1s",
+  },
+
+  '&:hover::before': {
+    animation: `${halftone} 1.5s forwards`,
+    opacity: 1,
+  },
+
+  '&:hover': {
+    color: theme.palette.primary.contrastText,
+    backgroundColor: theme.palette.secondary.main,
+    borderColor: theme.palette.primary.light,
+    transitionDelay: '0.2s'
+  },
+
+  '&:active': {
+    transform: "translateY(2px)",
+    borderBottomWidth: "2px"
+  },
+
+  '& > *': {
+    position: "relative",
+    zIndex: 1,
+  }
+}));
+
+const StyledButton: React.FC<StyledButtonProps> = ({ children }) => {
+  return (
+    <CustomButton>
+      {children}
+    </CustomButton>
+  );
+};
+
+export default StyledButton;
